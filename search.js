94638852b918df215435638fdba730c65580216c
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-04 23:43:08 +0600

    searching

---
+++ search.js
@@ -0,0 +1,21 @@
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 1TB camera flashlight Phone',
+    'yellow laptop with black camera',
+    'Dell 1X59 Lenovo commercial yoga laptop',
+    'LG supernova laptop Dell',
+    'HTC low price Phone',
+    'purple color phone with Laptop'
+];
+
+const searching = 'laptop';
+
+// indexOf
+
+/* const output = [];
+for (const product of products){
+    // console.log(product);
+    if(product.indexOf(searching) == -1)
+    output.push(product)
+}
+console.log(output); */
