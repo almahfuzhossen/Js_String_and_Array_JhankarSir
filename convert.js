5eee512eb2acba064131b86f053b32f82b0752f4
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-04 12:38:39 +0600

    first commit

---
+++ convert.js
@@ -0,0 +1,13 @@
+const userInput = 'bLAckPink';
+const savedUserName = 'blackPinK';
+
+/* console.log(userInput.toLowerCase());
+
+console.log(savedUserName.toUpperCase()); */
+
+if ( userInput.toLowerCase() == savedUserName.toLowerCase()) {
+console.log('valid user')
+}
+else {
+    console.log('invalid user')
+}
