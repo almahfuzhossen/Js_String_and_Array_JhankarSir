2d589348d8f6958ea0aabb805e19dd54148fd4e4
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-05 16:32:45 +0600

    array exists

---
+++ array-exists.js
@@ -0,0 +1,15 @@
+function megaFriend(friends) {
+    if (Array.isArray(friends) == false) {
+        return 'Please provide an array';
+    }
+    let mega = friends[0];
+    for (const friend of friends) {
+        if (friend.length > mega.length) {
+            mega = friend;
+        }
+    }
+    return mega;
+}
+const friends = ['kutub', 'Lion', 'Shamol', 'Sabbir'];
+const myBigBuddy = megaFriend(1245);
+console.log(myBigBuddy);
