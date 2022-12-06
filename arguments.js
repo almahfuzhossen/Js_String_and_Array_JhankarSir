c57a9b00959598f861b6e048591650865feca01d
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-06 12:17:38 +0600

    arguments

--- arguments.js
+++ arguments.js
@@ -8,3 +8,13 @@ function addNumbers (num1, num2){
 
 const sum = addNumbers(23, 45, 67, 76,35, 6788, 34, 901);
 console.log(sum);
+
+function getFullName(firstName,lastName){
+    let fullName ='';
+    for(const part of arguments){
+        fullName = fullName + part + ' ';
+    }
+    return fullName;
+}
+const name = getFullName('Omuk', 'Sonket', 'Bin', 'Hanif', 'Sonket', 'Bin', 'Russel', 'bitt', 'Khoyam');
+console.log(name)
